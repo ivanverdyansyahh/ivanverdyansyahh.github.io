83beb269e9f813cabaa73e305504c5639995d0da
ivanverdyansyahh <ivanverdyansyah69@gmail.com>
2022-06-10 14:44:44 +0800

    Second Commit, turn off javascript

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
-const overlay_layer = document.querySelector('.section-overlay');
-const btn_overlay = document.querySelector('.portfolio .image-content .img-card .img-overlay .text-link');
-const btn_close = document.querySelector('.portfolio .image-content .img-card .section-overlay .close'); 
-const card_overlay = document.querySelector('.portfolio .image-content .img-card .section-overlay .card-content');
+// const overlay_layer = document.querySelector('.section-overlay');
+// const btn_overlay = document.querySelector('.portfolio .image-content .img-card .img-overlay .text-link');
+// const btn_close = document.querySelector('.portfolio .image-content .img-card .section-overlay .close'); 
+// const card_overlay = document.querySelector('.portfolio .image-content .img-card .section-overlay .card-content');
 
-btn_overlay.addEventListener('click', function() {
-    overlay_layer.classList.add('show');
-    overlay_layer.addEventListener('click', function(e) {
-        if( e.target.className == 'section-overlay show' ) {
-            overlay_layer.classList.remove('show');
-        }
-    })
-});
+// btn_overlay.addEventListener('click', function() {
+//     overlay_layer.classList.add('show');
+//     overlay_layer.addEventListener('click', function(e) {
+//         if( e.target.className == 'section-overlay show' ) {
+//             overlay_layer.classList.remove('show');
+//         }
+//     })
+// });
 
-btn_close.addEventListener('click', function() {
-    overlay_layer.classList.remove('show');
-});
+// btn_close.addEventListener('click', function() {
+//     overlay_layer.classList.remove('show');
+// });
